2d9304951d747d54adabba3e02f458984af6d400
JYOTIPM1999 <jyotipm1999@gmail.com>
2024-03-07 20:05:32 +0530

    Backend api issue

--- client/src/axios.jsx
+++ client/src/axios.jsx
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const makeRequest = axios.create({
   baseURL: "http://localhost:9500/api/",
-
   withCredentials: true,
 });
--- server/index.js
+++ server/index.js
@@ -18,10 +18,9 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
-// app.use(cors());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://insoc-api.vercel.app/"],
+    origin: ["http://localhost:3000"],
     withCredentials: true,
   })
 );
