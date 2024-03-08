8b8e7e527cf288d6e3ce5064d323170edb288bb0
JYOTIPM1999 <jyotipm1999@gmail.com>
2024-03-08 20:20:16 +0530

    dotenv file added

--- server/src/connect.js
+++ server/src/connect.js
@@ -1,8 +1,16 @@
 import mysql from "mysql";
 
+import dotenv from "dotenv";
+dotenv.config();
+// console.log(process.env.DB_HOST);
+
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "social",
+  // host: "localhost",
+  host: process.env.DB_HOST,
+  // user: "root",
+  user: process.env.DB_USERNAME,
+  // password: "",
+  password: process.env.DB_PASSWORD,
+  // database: "social",
+  database: process.env.DB_DBNAME,
 });
